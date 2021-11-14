8c9bdfa084234da22ef2e9a536f1a0454c275e52
salman9000 <salmanhanif133@gmail.com>
2021-11-15 03:59:56 +0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# react-sass-todo-backend
+
+### `npm install`
+
+Installs all the modules
+
+## `node index.js`
+
+Starts server at port 3001
+Make sure to start frontend aswell
+
---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+let todolist = [
+  { id: 0, text: "Get Eggs" },
+  { id: 1, text: "Get Bread" },
+  { id: 2, text: "Go for a walk walking" },
+];
+
+let todoId = 3;
+app.get("/", async (req, res) => {
+  res.status(200).send("Welcome");
+});
+
+app.get("/api/todoList", async (req, res) => {
+  try {
+    res.status(200).send(todolist);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. Refresh the page" });
+  }
+});
+
+app.post("/api/addtodo", async (req, res) => {
+  try {
+    // throw new Error('Something broke yet again! 😱')
+    todoId += 1;
+    todolist = [...todolist, { id: todoId, text: req.body.text }];
+    res.status(200).send(todolist);
+  } catch (e) {
+    return res.status(404).json({ message: "Something went wrong. Refresh the page" });
+  }
+});
+
+app.delete("/api/deletetodo/:todoId", async (req, res) => {
+  try {
+    const { todoId } = req.params;
+    todolist = todolist.filter((todo) => parseInt(todoId) !== todo.id);
+    res.status(200).send(todolist);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. Refresh the page" });
+  }
+});
+
+app.put("/api/updatetodo/:todoId", async (req, res) => {
+  try {
+    const { todoId } = req.params;
+    const { text } = req.body;
+    const todoArrayCopy = [...todolist];
+    const todoElement = todoArrayCopy.filter((todo) => todoId == todo.id)[0];
+    todoElement.text = text;
+    todolist = todoArrayCopy;
+    res.status(200).send(todolist);
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong. Refresh the page" });
+  }
+});
+const port = 3001;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
